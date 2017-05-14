cd9f3d78d3654cfb2413107d88c530e74284a471
Johnny Sprada <jsprada@gmail.com>
2017-05-14 16:09:26 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,47 +1,35 @@
 #!/bin/sh
 # Install and setup MPA on a fresh installed Debian based Raspberry Pi
 
-apt-get install -y iw wpasupplicant wireless-tools wireless-regdb crda --no-install-recommends
+apt-get update
 
-apt-get install -y  mpd git python3 python3-pip rpi.gpio mpc ncmpcpp --no-install-recommends
+#apt-get install -y iw wpasupplicant wireless-tools wireless-regdb crda --no-install-recommends
+
+apt-get install -y  $(cat packages) --no-install-recommends
 
 sudo pip3 install python-mpd2
+sudo pip3 install flask
 
 hostnamectl set-hostname mpa
 
-sudo cat << _EOF_ > /etc/hosts
-127.0.0.1       localhost
-127.0.0.1       mpa
-__EOF__
+rm /etc/hosts
+mv ./etc/hosts /etc/hosts
 
-sudo cat << _EOF_ > /etc/motd
-
-  _ __  _ __  __ _ 
- | '  \| '_ \/ _\` |
- |_|_|_| .__/\__,_|
-       |_|         
- 
- Music Player Appliance
-
- https://github.com/jsprada/mpa
-
-_EOF_
+rm /etc/motd
+mv ./etc/hosts/motd /etc/motd
 
 # Copy song to music dir
 sudo cp ~/mpa/music/* /var/lib/mpd/music/
 
 # set audio to play throuh 3.5mm headphone jack
-amixer cset numid=3 1
+# amixer cset numid=3 1
 
-git clone https://github.com/jsprada/mpdbuttons
-cd mpdbuttons
-sudo sh install.sh
+#git clone https://github.com/jsprada/mpdbuttons
+#cd mpdbuttons
+#sudo sh install.sh
 
-<<<<<<< HEAD
 sudo systemctl enable ssh.socket
 =======
 mpc update
 
->>>>>>> d50fbfc18ce644178ddd3584eb12f4e3cd021609
 systemctl reboot
-
