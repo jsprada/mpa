0bd09526811884710786c80ca5fd72bcabfd57df
jsprada <jsprada@gmail.com>
2017-07-26 18:33:22 +0000

    bug fix

--- tools/status.sh
+++ tools/status.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 MPD=$(systemctl is-failed mpd.service)
-BUTTONS=$(systemctl is-failed mpaweb.service)
-WEB=$(systemctl is-failed mpabuttonsd.service)
+BUTTONS=$(systemctl is-failed mpabuttonsd.service)
+WEB=$(systemctl is-failed mpaweb.service)
 
 MPD_ENABLED=$(systemctl is-enabled mpd.service)
-BUTTONS_ENABLED=$(systemctl is-enabled mpaweb.service)
-WEB_ENABLED=$(systemctl is-enabled mpabuttonsd.service)
+BUTTONS_ENABLED=$(systemctl is-enabled mpabuttonsd.service)
+WEB_ENABLED=$(systemctl is-enabled mpaweb.service)
 
 echo "---mpa status---"
 echo "mpd: $MPD, $MPD_ENABLED"
